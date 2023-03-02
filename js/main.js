5b63f608aa5158f3f085b4b357a78d8ae06dd2fd
alex.kulemin <alex.kulemin@sharp-dev.net>
2023-03-02 18:05:03 +0400

    build

---
+++ js/main.js
@@ -0,0 +1,4 @@
+
+
+console.log('XXXXXX')
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIlxuXG5jb25zb2xlLmxvZygnWFhYWFhYJykiXSwiZmlsZSI6Im1haW4uanMifQ==
